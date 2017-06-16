972ee563e0f52ec13c924e6fe239159c80725955
Yaron Malin <yaron1m@gmail.com>
2017-06-16 14:28:23 +0300

    ContactsSection

--- src/containers/LectureFormSections/ContactsSection.js
+++ src/containers/LectureFormSections/ContactsSection.js
@@ -16,83 +16,90 @@ class ContactsSection extends React.Component {
         const sectionLabels = labels.lectureForm.contactsSection;
 
         const style = {
-            table: {
-                height: 10,
+            header: {
+                displaySelectAll: false,
                 marginTop: 20,
             },
-            height:100
+            textBox: {
+                textAlign: "right !important"
+            },
+            height: 100
         };
 
         return (
-        <SectionBase title = {sectionLabels.sectionName}>
-            <Table
-                height = {style.height}>
-                <TableHeader>
-                    <TableRow>
-                        <TableHeaderColumn>{sectionLabels.fields.firstName}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.lastName}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.phone1}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.phone2}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.phoneExtension}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.email}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.fax}</TableHeaderColumn>
-                        <TableHeaderColumn>{sectionLabels.fields.job}</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableRowColumn>משה</TableRowColumn>
-                        <TableRowColumn>כהן</TableRowColumn>
-                        <TableRowColumn>03-53532215</TableRowColumn>
-                        <TableRowColumn>052-5415049</TableRowColumn>
-                        <TableRowColumn>352</TableRowColumn>
-                        <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
-                        <TableRowColumn>077-2255449</TableRowColumn>
-                        <TableRowColumn>מפתח אתרים</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>משה</TableRowColumn>
-                        <TableRowColumn>כהן</TableRowColumn>
-                        <TableRowColumn>03-53532215</TableRowColumn>
-                        <TableRowColumn>052-5415049</TableRowColumn>
-                        <TableRowColumn>352</TableRowColumn>
-                        <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
-                        <TableRowColumn>077-2255449</TableRowColumn>
-                        <TableRowColumn>מפתח אתרים</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>משה</TableRowColumn>
-                        <TableRowColumn>כהן</TableRowColumn>
-                        <TableRowColumn>03-53532215</TableRowColumn>
-                        <TableRowColumn>052-5415049</TableRowColumn>
-                        <TableRowColumn>352</TableRowColumn>
-                        <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
-                        <TableRowColumn>077-2255449</TableRowColumn>
-                        <TableRowColumn>מפתח אתרים</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>משה</TableRowColumn>
-                        <TableRowColumn>כהן</TableRowColumn>
-                        <TableRowColumn>03-53532215</TableRowColumn>
-                        <TableRowColumn>052-5415049</TableRowColumn>
-                        <TableRowColumn>352</TableRowColumn>
-                        <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
-                        <TableRowColumn>077-2255449</TableRowColumn>
-                        <TableRowColumn>מפתח אתרים</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>משה</TableRowColumn>
-                        <TableRowColumn>כהן</TableRowColumn>
-                        <TableRowColumn>03-53532215</TableRowColumn>
-                        <TableRowColumn>052-5415049</TableRowColumn>
-                        <TableRowColumn>352</TableRowColumn>
-                        <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
-                        <TableRowColumn>077-2255449</TableRowColumn>
-                        <TableRowColumn>מפתח אתרים</TableRowColumn>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </SectionBase>
+            <SectionBase title={sectionLabels.sectionName}>
+                <Table
+                    height={style.height}
+                    style={style.textBox}
+                >
+                    <TableHeader
+                        displaySelectAll={style.header.displaySelectAll}>
+                        <TableRow>
+                            <TableHeaderColumn>{sectionLabels.fields.firstName}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.lastName}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.phone1}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.phone2}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.phoneExtension}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.email}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.fax}</TableHeaderColumn>
+                            <TableHeaderColumn>{sectionLabels.fields.job}</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow className="tableRow"
+                        >
+                            <TableRowColumn >משה</TableRowColumn>
+                            <TableRowColumn>כהן</TableRowColumn>
+                            <TableRowColumn>03-53532215</TableRowColumn>
+                            <TableRowColumn>052-5415049</TableRowColumn>
+                            <TableRowColumn>352</TableRowColumn>
+                            <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
+                            <TableRowColumn>077-2255449</TableRowColumn>
+                            <TableRowColumn>מפתח אתרים</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>משה</TableRowColumn>
+                            <TableRowColumn>כהן</TableRowColumn>
+                            <TableRowColumn>03-53532215</TableRowColumn>
+                            <TableRowColumn>052-5415049</TableRowColumn>
+                            <TableRowColumn>352</TableRowColumn>
+                            <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
+                            <TableRowColumn>077-2255449</TableRowColumn>
+                            <TableRowColumn>מפתח אתרים</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>משה</TableRowColumn>
+                            <TableRowColumn>כהן</TableRowColumn>
+                            <TableRowColumn>03-53532215</TableRowColumn>
+                            <TableRowColumn>052-5415049</TableRowColumn>
+                            <TableRowColumn>352</TableRowColumn>
+                            <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
+                            <TableRowColumn>077-2255449</TableRowColumn>
+                            <TableRowColumn>מפתח אתרים</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>משה</TableRowColumn>
+                            <TableRowColumn>כהן</TableRowColumn>
+                            <TableRowColumn>03-53532215</TableRowColumn>
+                            <TableRowColumn>052-5415049</TableRowColumn>
+                            <TableRowColumn>352</TableRowColumn>
+                            <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
+                            <TableRowColumn>077-2255449</TableRowColumn>
+                            <TableRowColumn>מפתח אתרים</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>משה</TableRowColumn>
+                            <TableRowColumn>כהן</TableRowColumn>
+                            <TableRowColumn>03-53532215</TableRowColumn>
+                            <TableRowColumn>052-5415049</TableRowColumn>
+                            <TableRowColumn>352</TableRowColumn>
+                            <TableRowColumn>yaron1m@gmail.com</TableRowColumn>
+                            <TableRowColumn>077-2255449</TableRowColumn>
+                            <TableRowColumn>מפתח אתרים</TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </SectionBase>
         );
     }
 }
