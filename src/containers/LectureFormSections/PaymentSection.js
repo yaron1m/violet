ea31ae2622117adeae673d192dff3e342ab2befe
Yaron Malin <yaron1m@gmail.com>
2017-06-16 16:52:22 +0300

    PaymentSection flexBox

--- src/containers/LectureFormSections/PaymentSection.js
+++ src/containers/LectureFormSections/PaymentSection.js
@@ -5,34 +5,36 @@ import FormDatePicker from "../LectureFormFields/FormDatePicker";
 import FormTextField from "../LectureFormFields/FormTextField";
 
 
-class LectureDetailsSection extends React.Component {
+class PaymentSection extends React.Component {
 
     render() {
         const sectionLabels = labels.lectureForm.paymentSection;
-        // const style = {
-        //     toggle: {
-        //         maxWidth: 250
-        //     },
-        //     textField: {
-        //         marginLeft: 20,
-        //     },
-        //     floatingLabelText: {
-        //         color: black
-        //     }
-        // };
+        const style = {
+            flex: {
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                alignItems: "flex-end"
+            }
+        };
 
         return (
-            <SectionBase title={sectionLabels.sectionName}>
-                <FormTextField title={sectionLabels.fields.paymentConditions}/>
-                <FormTextField title={sectionLabels.fields.actualPayDay}/>
-                <FormTextField title={sectionLabels.fields.proformaInvoiceNumber}/>
-                <FormDatePicker title={sectionLabels.fields.proformaInvoiceDate}/>
-                <FormTextField title={sectionLabels.fields.taxInvoiceNumber}/>
-                <FormDatePicker title={sectionLabels.fields.taxInvoiceDate}/>
+            <SectionBase
+                title={sectionLabels.sectionName}
+            >
+                <div style={style.flex}>
+                    <FormTextField title={sectionLabels.fields.paymentConditions}/>
+                    <FormTextField title={sectionLabels.fields.actualPayDay}/>
+                    <FormTextField title={sectionLabels.fields.proformaInvoiceNumber}/>
+                    <FormDatePicker title={sectionLabels.fields.proformaInvoiceDate}/>
+                    <FormTextField title={sectionLabels.fields.taxInvoiceNumber}/>
+                    <FormDatePicker title={sectionLabels.fields.taxInvoiceDate}/>
+                </div>
+
 
             </SectionBase>
         );
     }
 }
 
-export default LectureDetailsSection;
+export default PaymentSection;
