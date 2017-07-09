e5cee78c3cf9d228aa77dc217e78e53a557806bd
Yaron Malin <yaron1m@gmail.com>
2017-07-09 00:58:49 -0700

    init firebase

---
+++ src/database.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYLZaVfwMoWhCBzvhO8qJjC-CzqRceR0c",
+    authDomain: "violet-36bed.firebaseapp.com",
+    databaseURL: "https://violet-36bed.firebaseio.com",
+    projectId: "violet-36bed",
+    storageBucket: "violet-36bed.appspot.com",
+    messagingSenderId: "259015014878"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default database = firebase.database();
