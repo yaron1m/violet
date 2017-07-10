d1dc03ad37564affb4d4bf0b41683f20cf9e2f12
Yaron Malin <yaron1m@gmail.com>
2017-07-10 10:41:19 +0300

    add org id to state

--- src/reducers/reducer-organizations.js
+++ src/reducers/reducer-organizations.js
@@ -16,12 +16,12 @@ export default (state = initialState, action) => {
         case RECEIVE_ORGANIZATIONS:
             return Object.assign({}, state, {
                 isFetching: false,
-                didInvalidate: false,
                 data: action.payload,
             });
 
         case LOAD_ORGANIZATION_TO_FORM:
             let newSelected = state.data[action.payload];
+            newSelected.index = action.payload;
             return Object.assign({}, state, {
                 selected: newSelected
             });
