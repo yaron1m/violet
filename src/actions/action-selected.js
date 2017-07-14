b7aaf50fe48f6fe791c9916d4e6efe3e58af533a
Yaron Malin <yaron1m@gmail.com>
2017-07-14 14:24:10 +0300

    bug fix

--- src/actions/action-selected.js
+++ src/actions/action-selected.js
@@ -1,11 +1,12 @@
 export const SEND_SELECTED_ORGANIZATION = "SEND_SELECTED_ORGANIZATION";
 export function sendSelectedOrganization(key, value) {
     return {
-        type: UPDATE_SELECTED_ORGANIZATION,
+        type: SEND_SELECTED_ORGANIZATION,
         key: key,
         payload: value
     };
 }
+
 
 export const SEND_SELECTED_ORDER = "SEND_SELECTED_ORDER";
 export function sendSelectedOrder(key, value) {
