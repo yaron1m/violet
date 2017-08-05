0e23d3f268a81ca2a5b3052f6b881c5ee4028317
Yaron Malin <yaron1m@gmail.com>
2017-08-05 17:38:13 +0300

    order status calculation

--- src/util/order-status.js
+++ src/util/order-status.js
@@ -1,11 +1,80 @@
 import * as Immutable from "seamless-immutable";
+import * as _ from "lodash";
 
-export default function calculateOrderStatus(order){
+const possibleStatus = ["contact", "offer", "order", "approvedOffer", "isExecuting", "executed",
+    "waitingPayment", "payed", "cancelled", "disapproved", "followUp"];
 
-    const status = "order";
+export default function calculateOrderStatus(order) {
 
+    let status;
+
+    for (let i = 0; i < possibleStatus.length; i++) {
+        if (meetsRequirements(order, possibleStatus[i])) {
+            status = possibleStatus[i];
+        } else {
+            break;
+        }
+    }
+
+    console.log(status);
 
     return Immutable.merge(order, {
         status: status
     });
+}
+
+function meetsRequirements(order, requirement) {
+
+    let lectureTimesDates;
+    let now;
+
+    switch (requirement) {
+        case "contact":
+            return true;
+
+        case "offer":
+            //return _.has(order, "organizationId") &&  _.has(order, "lectureTimes") && order.lectureTimes[0].topic;
+            //TODO check for organization id
+            return existsAndNotEmpty(order,"lectureTimes") && order.lectureTimes[0].topic;
+
+        case "order":
+            return Boolean(order.lectureTimes[0].date);
+
+        case "approvedOffer":
+            return existsAndNotEmpty(order,"orderApproved");
+
+        case "isExecuting":
+            lectureTimesDates = _.mapValues(order.lectureTimes, lectureTime => lectureTime.date);
+            now = new Date();
+            return _.some(lectureTimesDates, date => new Date(date) < now);
+
+        case "executed":
+            lectureTimesDates = _.mapValues(order.lectureTimes, lectureTime => lectureTime.date);
+            now = new Date();
+            return _.every(lectureTimesDates, date => new Date(date) < now);
+
+        case "waitingPayment":
+            return existsAndNotEmpty(order,"proformaInvoiceNumber") || existsAndNotEmpty(order,"taxInvoiceNumber");
+
+        case "payed":
+            return existsAndNotEmpty(order,"receiptNumber");
+
+        case "cancelled":
+            return false;
+
+        case "disapproved":
+            return false;
+
+        //Followup
+            
+        default:
+            return false;
+
+
+    }
+
+}
+
+function existsAndNotEmpty(order, key) {
+    return _.has(order, key) && order[key];
 }
