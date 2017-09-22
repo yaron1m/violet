b33d4a4c68fab07c7c0476a42de3a2413abd833d
Yaron Malin <yaron1m@gmail.com>
2017-09-22 13:51:30 +0300

    Hide dashboard tables

--- src/containers/pages/dashboard/index.js
+++ src/containers/pages/dashboard/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import InfoBoxes from './info-boxes';
-import Tables from './tables';
 
 export default class HomePage extends React.Component {
     render() {
         return (
             <div>
                 <InfoBoxes/>
-                <Tables/>
             </div>
         );
     }
