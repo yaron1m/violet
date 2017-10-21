d13b71422f76a5f0cd8ca186bcf9481e402f2cc3
Yaron Malin <yaron1m@gmail.com>
2017-10-21 17:26:13 +0300

    remove index.css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from "react-tap-event-plugin";
-import './index.css';
 import Routes from "./routes";
 import Store from './store'
 import {Provider} from 'react-redux';
