c85c2e879002e46a317440cd2a9117d28b1d9e81
Yaron Malin <yaron1m@gmail.com>
2017-11-08 00:43:14 +0200

    Check dispatch count

--- src/containers/pages/order-page/action-buttons/__tests__/save-order.spec.js
+++ src/containers/pages/order-page/action-buttons/__tests__/save-order.spec.js
@@ -1,10 +1,12 @@
 import React from 'react';
+//import jest from 'jest';
 import {shallow} from 'enzyme'
 import {SaveOrderButton} from '../save-order';
 import {orderPageLabels} from "../../../../../store/labels/pages/order-page-labels";
+import {SHOW_REQUIRED_FIELDS} from "../../../../../store/required-fields/action-types";
 
-function setup(props) {
-    const thisProps = {
+function setup(extraProps) {
+    const props = {
         labels: orderPageLabels,
         // organizations:{},
         selectedOrganization: {},
@@ -14,32 +16,44 @@ function setup(props) {
         // nextOrderId: 5,
         // nextOrganizationId: 10,
         // orderMissingFields: [],
-        dispatch: jest.fn,
-        ...props
+        dispatch: jest.fn(),
+        ...extraProps
     };
 
-    const enzymeWrapper = shallow(<SaveOrderButton {...thisProps} />);
+    const enzymeWrapper = shallow(<SaveOrderButton {...props} />);
 
     return {
+        enzymeWrapper,
         props,
-        enzymeWrapper
     };
 }
 
-describe('store/labels/selectors', () => {
+describe('save order button', () => {
 
     it('shouldSave - organization not selected - false', () => {
-        const {enzymeWrapper} = setup({isSelectedOrganization: false});
+        const {enzymeWrapper, props} = setup({
+            isSelectedOrganization: false,
+        });
 
         expect(enzymeWrapper.instance().shouldSave())
             .toBeFalsy();
+
+        expect(props.dispatch.mock.calls.length).toBe(1);
+        expect(props.dispatch.mock.calls[0][0].title).toBe(props.labels.dialog.noOrganizationSelectedTitle);
     });
 
     it('shouldSave - there are missing fields - false', () => {
-        const {enzymeWrapper} = setup({missingFields: ["field"]});
+        const {enzymeWrapper, props} = setup({
+            isSelectedOrganization: true,
+            orderMissingFields: ["field"]
+        });
 
         expect(enzymeWrapper.instance().shouldSave())
             .toBeFalsy();
+
+        expect(props.dispatch.mock.calls.length).toBe(2);
+        expect(props.dispatch.mock.calls[0][0].type).toBe(SHOW_REQUIRED_FIELDS);
+        expect(props.dispatch.mock.calls[1][0].title).toBe(props.labels.dialog.missingFieldsTitle);
     });
 
     it('shouldSave - all valid - true', () => {
