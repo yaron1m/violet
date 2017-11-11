e9a4e5ac2ce9d0d2b4b999364183f476dc3f5449
Yaron Malin <yaron1m@gmail.com>
2017-11-11 17:20:20 +0200

    contacts-print

--- src/containers/pages/print-order-page/sections/contacts-print.js
+++ src/containers/pages/print-order-page/sections/contacts-print.js
@@ -1,51 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import connect from "react-redux/es/connect/connect";
 import {getLabels} from "../../../../store/labels/reducer";
 import {getSelectedOrder} from "../../../../store/selected/reducer";
 import PrintField from "../../../../components/custom-components/order-print/print-field";
 import PrintSection from "../../../../components/custom-components/order-print/print-section";
 import {getKey} from "../../order-page/sections/contacts-section/contact-row";
-import Divider from 'material-ui/Divider';
 
 class ContactsPrintSection extends React.Component {
     render() {
         const fieldData = {
-            titles: this.props.labels.titles,
+            titles: this.props.labels.contacts.titles,
             values: this.props.selectedOrder,
         };
 
+        const isFinancial = this.props.isFinancial;
+
         return (
-            <PrintSection title={this.props.labels.sectionName}>
-                <PrintField data={fieldData} name={getKey("contactFirstName", false)}/>
-                <PrintField data={fieldData} name={getKey("contactLastName", false)}/>
-                <PrintField data={fieldData} name={getKey("contactJob", false)}/>
-                <PrintField data={fieldData} name={getKey("contactPhone1", false)}/>
-                <PrintField data={fieldData} name={getKey("contactEmail", false)}/>
-                <PrintField data={fieldData} name={getKey("contactPhone2", false)}/>
-                <PrintField data={fieldData} name={getKey("contactPhoneExtension", false)}/>
-                <PrintField data={fieldData} name={getKey("contactFax", false)}/>
-
-                <Divider style={{marginTop: 10, marginBottom: 10}}/>
-
-                <PrintField data={fieldData} name={getKey("contactFirstName", true)}/>
-                <PrintField data={fieldData} name={getKey("contactLastName", true)}/>
-                <PrintField data={fieldData} name={getKey("contactJob", true)}/>
-                <PrintField data={fieldData} name={getKey("contactPhone1", true)}/>
-                <PrintField data={fieldData} name={getKey("contactEmail", true)}/>
-                <PrintField data={fieldData} name={getKey("contactPhone2", true)}/>
-                <PrintField data={fieldData} name={getKey("contactPhoneExtension", true)}/>
-                <PrintField data={fieldData} name={getKey("contactFax", true)}/>
-
+            <PrintSection
+                title={isFinancial ? this.props.labels.payment.financialContactTitle : this.props.labels.contacts.sectionName}
+            >
+                <PrintField data={fieldData} name={getKey("contactFirstName", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactLastName", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactJob", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactPhone1", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactEmail", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactPhone2", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactPhoneExtension", isFinancial)}/>
+                <PrintField data={fieldData} name={getKey("contactFax", isFinancial)}/>
             </PrintSection>
         );
     }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state, ownProps) {
     return {
-        labels: getLabels(state).pages.orderPage.sections.contacts,
+        labels: getLabels(state).pages.orderPage.sections,
         selectedOrder: getSelectedOrder(state),
+        isFinancial: ownProps.isFinancial
     };
 }
+
+ContactsPrintSection.propTypes = {
+    isFinancial: PropTypes.bool
+};
 
 export default connect(mapStateToProps)(ContactsPrintSection);
