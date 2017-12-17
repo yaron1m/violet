faf67df18db6008533aa1510c212e5f2d0258266
Yaron Malin <yaron1m@gmail.com>
2017-12-17 20:58:50 +0200

    remove uneeded code

--- src/containers/pages/order-page/action-buttons/__tests__/save-order.spec.js
+++ src/containers/pages/order-page/action-buttons/__tests__/save-order.spec.js
@@ -7,14 +7,7 @@ import {SHOW_REQUIRED_FIELDS} from "../../../../../store/required-fields/action-
 function setup(extraProps) {
     const props = {
         labels: orderPageLabels,
-        // organizations:{},
         selectedOrganization: {},
-        // isSelectedOrganization: false,
-        // selectedOrder: {},
-        // isSelectedOrder: false,
-        // nextOrderId: 5,
-        // nextOrganizationId: 10,
-        // orderMissingFields: [],
         dispatch: jest.fn(),
         ...extraProps
     };
