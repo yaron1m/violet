e5e995699062d1497e4dddffd5c4c63591647d7a
Yaron Malin <yaron1m@gmail.com>
2018-03-13 13:57:13 +0200

    Move files

---
+++ src/containers/Pages/OrderPage/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import OrderPageSections from './Sections';
+import OrderPageTitle from './order-page-title';
+import OrderActionButtons from './action-buttons/index';
+import OrderTimes from './order-page-footer'
+
+export default class OrderForm extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <OrderPageTitle/>
+                <OrderTimes/>
+
+                <OrderActionButtons/>
+
+                <OrderPageSections/>
+            </div>
+        );
+    }
+}
