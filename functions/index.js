b4d0844314f07897d98afdd5dca2f2c8568eaad6
Yaron Malin <yaron1m@gmail.com>
2018-03-21 00:56:20 +0200

    Test function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const ref = admin.database().ref("orders");
+
+exports.updateStatus = functions.https.onRequest((request, response) => {
+
+
+    ref.once('value').then(snapshot => {
+        const updatedOrders = {};
+        console.log("snapshot:" + JSON.stringify(snapshot));
+
+        snapshot.forEach(order =>{
+            const thisOrder=order.val();
+            console.log("thisOrder:" + thisOrder);
+            thisOrder.nextId= parseInt(thisOrder.id) + 1;
+            console.log("thisOrder After:" + JSON.stringify(thisOrder));
+            updatedOrders[order.key] = thisOrder;
+        });
+
+        console.log("updatedOrders:");
+        console.log(updatedOrders);
+
+        return updatedOrders;
+    }).then(updatedOrders => {
+        ref.update(updatedOrders).then(() => {
+            response.send("done");
+        }).catch( error => {
+            response.send(error);
+        });
+    }).catch( error => {
+        response.send(error);
+    })
+
+
+});
+
+
