d653ad0ad966e67a1d48f2ea5ee025463c643e7e
Yaron Malin <yaron1m@gmail.com>
2018-05-15 18:51:18 +0300

    Add delete participant button

--- src/containers/Pages/OrderPage/Sections/LectureDetailsSections/PublicCourse/PublicCourseParticipant.js
+++ src/containers/Pages/OrderPage/Sections/LectureDetailsSections/PublicCourse/PublicCourseParticipant.js
@@ -7,45 +7,52 @@ import {
 import Sizes from "../../../../../../util/consts/sizes";
 import _ from 'lodash';
 import PropTypes from "prop-types";
+import {IconButton} from "material-ui";
+import DeleteIcon from 'material-ui-icons/Delete';
 
 export default class PublicCourseParticipant extends React.Component {
 
     render() {
+        const participantId = this.props.participantId;
+
         return (
             <CustomPaper
                 title={this.props.sectionName}
             >
                 <div style={flexStyle}>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId}
+                    <PublicCourseParticipantsCustomText participantIndex={participantId}
                                                         name="participantFirstName"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId}
+                    <PublicCourseParticipantsCustomText participantIndex={participantId}
                                                         name="participantLastName"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId}
+                    <PublicCourseParticipantsCustomText participantIndex={participantId}
                                                         name="participantEnglishFirstName"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId}
+                    <PublicCourseParticipantsCustomText participantIndex={participantId}
                                                         name="participantEnglishLastName"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId} name="idNumber"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId} name="phone"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId} name="job"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId} name="email"
+                    <PublicCourseParticipantsCustomText participantIndex={participantId} name="idNumber"/>
+                    <PublicCourseParticipantsCustomText participantIndex={participantId} name="phone"/>
+                    <PublicCourseParticipantsCustomText participantIndex={participantId} name="job"/>
+                    <PublicCourseParticipantsCustomText participantIndex={participantId} name="email"
                                                         size={Sizes.XL}/>
-                    <PublicCourseParticipantsCustomCheckBox participantIndex={this.props.participantId}
+                    <PublicCourseParticipantsCustomCheckBox participantIndex={participantId}
                                                             name="isqMember"/>
-                    <PublicCourseParticipantsCustomText participantIndex={this.props.participantId} name="participantCost"/>
+                    <PublicCourseParticipantsCustomText participantIndex={participantId} name="participantCost"/>
+                    <IconButton onClick={this.props.onDelete}>
+                        <DeleteIcon/>
+                    </IconButton>
                 </div>
                 <div>
                     {_.map(_.orderBy(this.props.selectedPublicCourseLectures, x => x.date),
                         lecture => {
-                        const key = "attendingLecture" + lecture.id;
-                        return (
-                            <PublicCourseParticipantsCustomCheckBox
-                                key={key}
-                                participantIndex={this.props.participantId}
-                                name={"attendingLecture" + lecture.id}
-                                titles={{[key]: new Date(lecture.date).toLocaleDateString() + " - " + lecture.topic}}
-                            />
-                        )
-                    })}
+                            const key = "attendingLecture" + lecture.id;
+                            return (
+                                <PublicCourseParticipantsCustomCheckBox
+                                    key={key}
+                                    participantIndex={participantId}
+                                    name={"attendingLecture" + lecture.id}
+                                    titles={{[key]: new Date(lecture.date).toLocaleDateString() + " - " + lecture.topic}}
+                                />
+                            )
+                        })}
                 </div>
             </CustomPaper>
         );
@@ -56,4 +63,5 @@ PublicCourseParticipant.propTypes = {
     sectionName: PropTypes.string.isRequired,
     participantId: PropTypes.number.isRequired,
     selectedPublicCourseLectures: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired,
 };
