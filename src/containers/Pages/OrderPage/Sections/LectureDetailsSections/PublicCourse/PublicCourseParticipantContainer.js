c2eb5874bcfa8757a3614fc661f6c5c25698fe14
Yaron Malin <yaron1m@gmail.com>
2018-05-15 19:02:12 +0300

    Add onDelete function

--- src/containers/Pages/OrderPage/Sections/LectureDetailsSections/PublicCourse/PublicCourseParticipantContainer.js
+++ src/containers/Pages/OrderPage/Sections/LectureDetailsSections/PublicCourse/PublicCourseParticipantContainer.js
@@ -1,16 +1,40 @@
 import {connect} from 'react-redux';
 import PropTypes from "prop-types";
 import PublicCourseParticipant from "./PublicCourseParticipant";
-import {getSelectedPublicCourse} from "../../../../../../store/selected/reducer";
+import {getSelectedOrder, getSelectedPublicCourse} from "../../../../../../store/selected/reducer";
+import * as Immutable from "seamless-immutable";
+import {updateSelectedOrder} from "../../../../../../store/selected/actions";
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
     return {
-        participantId: ownProps.participantId,
         selectedPublicCourseLectures: getSelectedPublicCourse(state) ? getSelectedPublicCourse(state).lectures : [],
+        selectedOrder: getSelectedOrder(state),
     };
 }
 
-const Container = connect(mapStateToProps)(PublicCourseParticipant);
+function mapDispatchToProps(dispatch) {
+    return {
+        updateSelectedOrder: (key, value) => dispatch(updateSelectedOrder(key, value)),
+    };
+}
+
+function removeParticipant(selectedOrder, updateSelectedOrder, participantId) {
+    const thisSelectedOrder = Immutable.asMutable(selectedOrder, {deep: true});
+    const publicCourseParticipants = thisSelectedOrder.publicCourseParticipants;
+    publicCourseParticipants.splice(participantId, 1);
+
+    updateSelectedOrder("publicCourseParticipants", publicCourseParticipants);
+}
+
+function mergeProps(stateProps, dispatchProps, ownProps) {
+    return {
+        participantId: ownProps.participantId,
+        selectedPublicCourseLectures: stateProps.selectedPublicCourseLectures,
+        onDelete: () => removeParticipant(stateProps.selectedOrder, dispatchProps.updateSelectedOrder, ownProps.participantId)
+    }
+}
+
+const Container = connect(mapStateToProps, mapDispatchToProps, mergeProps)(PublicCourseParticipant);
 
 Container.propTypes = {
     participantId: PropTypes.number.isRequired,
