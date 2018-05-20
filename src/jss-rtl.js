9248fb55d5ad96631f2c93c24ffe10f6996c8129
Yaron Malin <yaron1m@gmail.com>
2018-05-20 18:48:23 +0300

    fix lint issues

--- src/jss-rtl.js
+++ src/jss-rtl.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { create } from 'jss';
+import {create} from 'jss';
 import rtl from 'jss-rtl';
 import JssProvider from 'react-jss/lib/JssProvider';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import {createGenerateClassName, jssPreset} from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 // Configure JSS
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const jss = create({plugins: [...jssPreset().plugins, rtl()]});
 
 // Custom Material-UI class name generator.
 const generateClassName = createGenerateClassName();
 
-export default function RTL(props) {
+const provider = function RTL(props) {
     return (
         <JssProvider jss={jss} generateClassName={generateClassName}>
             {props.children}
         </JssProvider>
     );
 }
+
+provider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default provider;
