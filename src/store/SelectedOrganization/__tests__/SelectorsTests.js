420052f954f6c25e18071e36a9f84a6398b96678
Yaron Malin <yaron1m@gmail.com>
2018-05-28 23:06:09 +0300

    remove dead code

--- src/store/SelectedOrganization/__tests__/SelectorsTests.js
+++ src/store/SelectedOrganization/__tests__/SelectorsTests.js
@@ -5,22 +5,6 @@ const sampleState = {
         organization: {
             "address": "האחות חיה 4, רמת גן",
             "companyId": "514815745",
-            "contacts": [{
-                "email": "hanan@c-point.co.il",
-                "fax": "072-2244660",
-                "firstName": "חנן",
-                "job": "מנכ\"ל",
-                "lastName": "מלין",
-                "phone1": "052-2775072",
-                "phone2": "03-5354045",
-                "phoneExtension": 10
-            }, {
-                "email": "yaron1m@gmail.com",
-                "firstName": "ירון",
-                "job": "מנכ\"ל",
-                "lastName": "מלין",
-                "phone1": "052-5415049"
-            }],
             "howReachedUs": "מפה לאוזן",
             "id": 0,
             "name": "שיא האיכות בע\"מ"
