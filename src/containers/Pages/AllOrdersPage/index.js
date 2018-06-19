6aa78235c36bdb15fd5a7711f556059c04803c9b
Yaron Malin <yaron1m@gmail.com>
2018-06-19 17:27:01 +0300

    Show all orders

--- src/containers/Pages/AllOrdersPage/index.js
+++ src/containers/Pages/AllOrdersPage/index.js
@@ -1,33 +1,57 @@
 import React from 'react';
 import AllOrdersTableContainer from './AllOrdersTableContainer';
 import FilterStatusSelectField from './FilterStatusSelectField';
+import CustomToggle from "../../../components/CustomComponents/CustomToggle";
+import {updateObject} from "../../../util/ObjectUpdater";
+import {flexStyle} from "../../../components/CustomComponents/CustomPaper";
 
 export default class AllOrdersPage extends React.Component {
     constructor() {
         super();
         this.state = {
             filterStatus: null,
+            showAll: false,
         };
     }
 
     updateStatus(status) {
-        this.setState({
+        this.setState(updateObject(this.state, {
             filterStatus: status,
-        })
+        }));
+    }
+
+    updateShowAll(key, value) {
+        this.setState(updateObject(this.state, {
+            showAll: value,
+        }))
     }
 
     render() {
         const filterStatusSelectField =
-            <FilterStatusSelectField
-                updateStatus={this.updateStatus.bind(this)}
-                filterStatus={this.state.filterStatus}
-            />;
+            <div style={flexStyle}>
+                <FilterStatusSelectField
+                    updateStatus={this.updateStatus.bind(this)}
+                    filterStatus={this.state.filterStatus}
+                />
+                <CustomToggle
+                    name="showAll"
+                    titles={{showAll: "הצג הכל"}}
+                    updateAction={this.updateShowAll.bind(this)}
+                    values={{showAll: this.state.showAll}}
+                />
+            </div>;
 
         return (
             <AllOrdersTableContainer
                 filterStatus={this.state.filterStatus}
                 beforeTable={filterStatusSelectField}
+                limit={getLimit(this.state.showAll)}
             />
         );
     }
+}
+
+function getLimit(showAll) {
+    const limit = 30;
+    return showAll ? -1 : limit;
 }
