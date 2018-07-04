1ee27889843fa718d16fc1f15614574a141ab449
Yaron Malin <yaron1m@gmail.com>
2018-07-04 21:25:37 +0300

    add tests

---
+++ src/store/Lists/__tests__/ActionsTests.js
@@ -0,0 +1,13 @@
+import * as actions from '../actions'
+import * as actionTypes from '../ActionTypes'
+
+describe('Lists reducer actions', () => {
+    it('should create an action of received lists', () => {
+        const lists = {blah: "hello"};
+        const expectedAction = {
+            type: actionTypes.RECEIVE_LISTS,
+            payload: lists,
+        };
+        expect(actions.receiveLists(lists)).toEqual(expectedAction)
+    })
+});
