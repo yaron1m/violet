90ab5d3bb74e2ee912df449e4ab6a5a6095949c7
Yaron Malin <yaron1m@gmail.com>
2018-07-28 12:04:16 +0300

    Move client side app to client folder

---
+++ client/src/components/CustomComponents/__tests__/AbstractCustomFieldTests.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import AbstractCustomField from "../AbstractCustomField";
+import Sizes from "../../../util/Constants/Sizes";
+
+function setup(otherProps) {
+    const labels = {
+        titles: {
+            organizationName: "שם הארגון",
+        },
+    };
+
+    const props = {
+        name: "organizationName",
+        titles: labels.titles,
+        values: {
+            organizationName: "Google",
+        },
+        requiredFields: ["someField"],
+        updateAction: jest.fn(),
+        size: Sizes.XL,
+        ...otherProps,
+    };
+
+    return new AbstractCustomField(props);
+
+}
+
+describe('Abstract Field Class', () => {
+
+    it('Constructor - fields are correct', () => {
+        const target = setup({});
+
+        expect(target.name).toEqual("organizationName");
+        expect(target.title).toEqual("שם הארגון");
+        expect(target.state).toEqual({value: "Google"});
+        expect(target.basicStyle).toEqual({
+            marginRight: 20,
+            marginBottom: 10,
+            verticalAlign: "bottom",
+        });
+    });
+
+    it('GetWidth - correct', () => {
+        expect(setup().width).toEqual(200);
+        expect(setup({size: Sizes.S}).width).toEqual(50);
+    });
+
+    it('GetWidth - invalid size - return default', () => {
+        const target = setup({size: "hello"});
+
+        expect(target.width).toEqual(150);
+    });
+
+    it('validateProps - missing title - throws exception', () => {
+        expect(() => setup({name: "hello"})).toThrow();
+    });
+
+    it('validateProps - missing updateAction - throws exception', () => {
+        expect(() => setup({updateAction: null})).toThrow();
+    });
+
+    it('componentWillReceiveProps - value not changed - state not changed', () => {
+        const target = setup();
+        target.setState = jest.fn();
+
+        const newProps = {
+            values: {
+                organizationName: "Google",
+            },
+        };
+        target.componentWillReceiveProps(newProps);
+
+        expect(target.setState.mock.calls).toHaveLength(0);
+    });
+
+
+    it('componentWillReceiveProps - value changed - state changed', () => {
+        const target = setup();
+        target.setState = jest.fn();
+
+        const newProps = {
+            values: {
+                organizationName: "Amazon",
+            },
+        };
+        target.componentWillReceiveProps(newProps);
+
+        expect(target.setState.mock.calls).toHaveLength(1);
+        expect(target.setState.mock.calls[0][0].value).toBe("Amazon");
+    });
+
+    it('componentWillReceiveProps - value changed to undefined - state changed to empty string', () => {
+        const target = setup();
+        target.setState = jest.fn();
+
+        const newProps = {
+            values: {},
+        };
+        target.componentWillReceiveProps(newProps);
+
+        expect(target.setState.mock.calls).toHaveLength(1);
+        expect(target.setState.mock.calls[0][0].value).toBe("");
+    });
+
+    it('handleChange - new value - called with the name', () => {
+        const updateAction = jest.fn();
+        const target = setup({updateAction});
+
+        target.handleChange("Amazon");
+
+        expect(updateAction.mock.calls).toHaveLength(1);
+        expect(updateAction.mock.calls[0]).toEqual(["organizationName", "Amazon"]);
+    });
+
+    it('shouldShowError - field is not required - false', () => {
+        expect(setup().shouldShowError()).toBeFalsy();
+    });
+
+    it('shouldShowError - required but has value - false', () => {
+        expect(setup({requiredFields: ["organizationName"]}).shouldShowError()).toBeFalsy();
+    });
+
+    it('shouldShowError - required with no value - true', () => {
+        expect(setup({
+            requiredFields: ["organizationName"],
+            values: {}
+        }).shouldShowError()).toBeTruthy();
+    });
+});
---
+++ client/src/containers/Pages/FutureLecturesPage/__tests__/FutureLecturesPageTests.js
@@ -0,0 +1,78 @@
+import * as Target from "../index";
+import Status from "../../../../util/Constants/Status";
+
+function getState(orders) {
+    return {
+        orders,
+        organizations: {
+            100: {
+                organizationId: 100,
+                organizationName: "OrgA"
+            }
+        }
+    };
+}
+
+describe('FutureLecturesPage', () => {
+    it('getFutureLectureTimes - choose organization - load organization action', () => {
+        const now = new Date();
+        const yesterday = new Date();
+        yesterday.setDate(now.getDate() - 1);
+        yesterday.setHours(0, 0, 0, 0);
+
+        const thisMorning = new Date();
+        thisMorning.setHours(0, 0, 0, 0);
+
+        const tomorrow = new Date();
+        tomorrow.setDate(now.getDate() + 1);
+
+        const orders = {
+            "1000": {
+                "id": 1000,
+                "lectureTimes": [
+                    {
+                        topic: "3 days ago",
+                        date: new Date().setDate(now.getDate() - 3)
+                    },
+                    {
+                        topic: "yesterday",
+                        date: yesterday
+                    },
+                    {
+                        topic: "this morning",
+                        date: thisMorning
+                    },
+                    {
+                        topic: "tomorrow",
+                        date: tomorrow
+                    },
+                ],
+                "status": Status.approvedOrder,
+                organizationId: 100,
+            },
+            "1001": {
+                "id": 1001,
+                "lectureTimes": [
+                    {
+                        topic: "lecture1",
+                        date: new Date()
+                    },
+                    {
+                        topic: "other lecture",
+                        date: new Date()
+                    }
+                ],
+                "status": Status.contact,
+                organizationId: 100,
+            },
+        };
+
+        const result = Target.getFutureLectureTimes(getState(orders));
+
+        expect(result).toHaveLength(2);
+        expect(result[0].topic).toEqual("this morning");
+        expect(result[1].topic).toEqual("tomorrow");
+    });
+});
+
+
