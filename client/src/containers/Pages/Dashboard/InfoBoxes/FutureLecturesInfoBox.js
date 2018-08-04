f402dd87f67571f9d0185496175ca069c88f26ed
Yaron Malin <yaron1m@gmail.com>
2018-08-04 20:38:33 +0300

    merge functions call

--- client/src/containers/Pages/Dashboard/InfoBoxes/FutureLecturesInfoBox.js
+++ client/src/containers/Pages/Dashboard/InfoBoxes/FutureLecturesInfoBox.js
@@ -1,22 +1,18 @@
 import {connect} from "react-redux";
 import {getLabels} from "../../../../store/Labels/Reducer";
 import {redirect} from "../../../../util/HistoryUtil";
-import {getAllLectureTimes} from "../../../../store/orders/selectors";
-import * as _ from 'lodash';
 import {isFetching} from "../../../../store/Firebase/Reducer";
-import Status from "../../../../util/Constants/Status";
 import CheckBoxIcon from '@material-ui/icons/CheckCircle';
 import InfoBox from "./InfoBox";
 import Colors from "../../../../util/Constants/Colors";
+import {getFutureLectureTimes} from "../../FutureLecturesPage";
 
 function calculateFutureLectures(state) {
     if (isFetching(state))
         return;
 
-    const now = new Date();
-    const lectureTimes = getAllLectureTimes(state, [Status.approvedOrder, Status.isExecuting]);
-    const futureLectureTimes = _.filter(lectureTimes, lectureTime => new Date(lectureTime.date) > now);
-    return futureLectureTimes.length.toString();
+    const lectureTimes = getFutureLectureTimes(state);
+    return lectureTimes.length.toString();
 }
 
 
