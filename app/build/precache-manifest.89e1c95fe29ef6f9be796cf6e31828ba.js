f2ba863362f5efc646d75b632a6991bb134a039e
Yaron Malin <yaron1m@gmail.com>
2018-11-02 00:50:30 +0200

    Change client folder to app

---
+++ app/build/precache-manifest.89e1c95fe29ef6f9be796cf6e31828ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c5682a817faaceb842a",
+    "url": "/static/js/main.8c5682a8.chunk.js"
+  },
+  {
+    "revision": "e4ecb9a1efa37d5ebd14",
+    "url": "/static/js/1.e4ecb9a1.chunk.js"
+  },
+  {
+    "revision": "cf226d80a2eed249e917bcddbacad1fb",
+    "url": "/index.html"
+  }
+];
