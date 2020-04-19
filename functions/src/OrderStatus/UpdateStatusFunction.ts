5365b2db26a1963b16550636c43202c05eae58d3
Yaron Malin <yaron1m@gmail.com>
2020-04-19 20:09:59 +0300

    Functions use common order status calculator

--- functions/src/OrderStatus/UpdateStatusFunction.ts
+++ functions/src/OrderStatus/UpdateStatusFunction.ts
@@ -1,8 +1,7 @@
 import * as _ from 'lodash';
 import * as express from "express";
 import * as admin from "firebase-admin";
-import {IOrder, IPublicCourse} from "../Common";
-import {calculateOrderStatus} from "./OrderStatusCalculator";
+import {IOrder, IPublicCourse, calculateOrderStatus, isPublicCourseOrder} from "../Common";
 
 export default async function (
     request: express.Request,
@@ -12,14 +11,19 @@ export default async function (
 ) {
     try {
         const publicCoursesSnapshot = await publicCoursesRef.once('value');
-        const publicCourses = publicCoursesSnapshot.val() as IPublicCourse;
+        const publicCourses = publicCoursesSnapshot.val() as IPublicCourse[];
         const ordersSnapshot = await ordersRef.once('value');
 
         const updatedOrders : {[id: number]: IOrder} = {};
 
         ordersSnapshot.forEach(order => {
             const thisOrder = order.val() as IOrder;
-            const calculatedStatus = calculateOrderStatus(thisOrder, publicCourses);
+            let publicCourse: IPublicCourse;
+            if (isPublicCourseOrder(thisOrder)) {
+                publicCourse = publicCourses[thisOrder.publicCourseId];
+            }
+
+            const calculatedStatus = calculateOrderStatus(thisOrder, publicCourse);
 
             if (!_.startsWith(calculatedStatus, thisOrder.status)) {
                 console.log("id: " + thisOrder.id + ", old status: " + thisOrder.status + ", new status: " + calculatedStatus);
